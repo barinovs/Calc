854da1c317ede31810454230651c0d715cedd219
barinovs <barinovserge@gmail.com>
2020-02-09 02:09:09 +0300

    Инпуты в процессе

--- src/components/inputWithBtns/inputWithBtns.js
+++ src/components/inputWithBtns/inputWithBtns.js
@@ -6,17 +6,89 @@ import './index.css'
 class InputWithBtns extends Component{
     constructor(props) {
         super(props)
+        this.state = {
+            mod: 0,
+            inpText: ""
+        }
+
+        this.input = React.createRef();
+
+        this.increaseMod = this.increaseMod.bind(this)
+        this.decreaseMod = this.decreaseMod.bind(this)
+        this.updateText = this.updateText.bind(this)
+        this.changeInpText = this.changeInpText.bind(this)
+    }
+
+    changeInpText(textInp) {
+        console.log('textInp ', textInp);
+        let mod = (textInp / this.props.koef).toFixed()
+        this.setState({mod})
+    }
+
+    updateText(mod) {
+        let inpText = ""
+        if (mod > 0) {
+            inpText = (mod * this.props.koef).toFixed(3)
+            this.setState( state => {
+                return {inpText: inpText}
+            })
+        }else{
+            inpText = "доделать"
+        }
+        this.input.current.value = inpText
+    }
+
+    increaseMod() {
+        let mod = this.state.mod + 1
+        this.setState( state => {
+            return {mod: state.mod + 1}
+        } )
+
+        this.updateText(mod)
+
+    }
+
+    decreaseMod() {
+        let mod = this.state.mod - 1
+        this.setState( state => {
+            if (state.mod > 1) {
+                return {mod: state.mod - 1}
+            }else{
+                return {mod: 1}
+            }
+
+        } )
+        this.updateText(mod)
     }
 
     render() {
+        const { mod, inpText } = this.state
         return(
-            <div className="input-group my-touchspin bootstrap-touchspin-injected">
-                <input className="touchspin-vertical form-control"></input>
+            <div>
+                <div className="input-group my-touchspin bootstrap-touchspin-injected">
+                    <input
+                        type="text"
+                        className="touchspin-vertical form-control"
+                        ref={this.input}
+                        onChange={ (e) => this.changeInpText(e.target.value) }
 
-                <span className="input-group-btn-vertical-my">
-                    <button className="btn btn-primary bootstrap-touchspin-up-my icon-arrow-up22"></button>
-                    <button className="btn btn-primary bootstrap-touchspin-down-my icon-arrow-down22"></button>
-                </span>
+                        defaultValue=""
+                    ></input>
+
+                    <span className="input-group-btn-vertical-my">
+                        <button
+                            className="btn btn-primary bootstrap-touchspin-up-my icon-arrow-up22"
+                            onClick={ () => this.increaseMod()}
+                        >
+                        </button>
+                        <button
+                            className="btn btn-primary bootstrap-touchspin-down-my icon-arrow-down22"
+                            onClick={ () => this.decreaseMod()}
+                        >
+                        </button>
+                    </span>
+                </div>
+                <div className="shift">{mod} мод</div>
             </div>
         )
     }
--- src/components/sizeOfScreen/sizeOfScreen.js
+++ src/components/sizeOfScreen/sizeOfScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
 import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import InputWithBtns from '../inputWithBtns/inputWithBtns'
 
 class SizeOfScreen extends Component{
@@ -11,7 +14,15 @@ class SizeOfScreen extends Component{
     render() {
         return(
             <div>Размер экрана
-                <InputWithBtns />
+                <Row>
+                    <Col lg="6">
+                        <InputWithBtns koef={0.32}/>
+                    </Col>
+                    <Col lg="6">
+                        <InputWithBtns koef={0.16}/>
+                    </Col>
+                </Row>
+
             </div>
         )
     }
