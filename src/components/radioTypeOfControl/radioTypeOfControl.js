008e5ef01bcc50d748ee0e7d3f67e18ff6c71e11
Баринов Сергей Николаевич <barinovsn@OSZNV.ru>
2020-02-11 19:01:02 +0300

    ДОделал after

--- src/components/radioTypeOfControl/radioTypeOfControl.js
+++ src/components/radioTypeOfControl/radioTypeOfControl.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
-
+import styled, { css }  from 'styled-components';
 
 import Form from 'react-bootstrap/Form'
 
 import './index.css'
+
+const Indicator = styled.div`
+    position: absolute;
+    top: 2px;
+    left: 0;
+    height: 19px;
+    width: 19px;
+    border: 1px solid #107bb1;
+    border-radius: 50%;
+    
+`
+const IndicatorWithAfter = styled.div`
+    position: absolute;
+    top: 2px;
+    left: 0;
+    height: 19px;
+    width: 19px;
+    border: 1px solid #107bb1;
+    border-radius: 50%;
+    background: #107bb1;
+    &:after {
+        left: 6px;
+        top: 6px;
+        height: 6px;
+        width: 6px;
+        border-radius: 50%;
+        background: #ffffff;
+        box-sizing: unset;
+        content: '';
+        position: absolute;
+    }
+`
 
 class RadioTypeOfControl extends Component {
     constructor(props) {
@@ -22,28 +54,42 @@ class RadioTypeOfControl extends Component {
 
     render() {
         const {typesOfControl} = this.props
+        const _style = {
+            "before":{
+            boxSizing: "unset",
+            content: '',
+            position: "absolute",
+            display: "none"}
+        }
         return(
             <div style={{display: 'block'}}>
                 <Form>
                     {
                         typesOfControl.map( (item) => {
+                            const beforeClass = item.value == this.state.value
                             return (
-                              <Form.Check
-                                type="radio"
-                                id="t"
-                                key={item.id}
+
+                                  <Form.Check
+                                    type="radio"
+                                    id="t"
+                                    key={item.id}
 
 
 
-                              >
-                                <Form.Check.Input
-                                  type="radio"
-                                  value={item.value}
-                                  checked={item.value == this.state.value}
-                                  onChange={ (e) => this._change(e.target.value) }
-                                />
-                                <Form.Check.Label>{item.name}</Form.Check.Label>
-                              </Form.Check>
+                                  >
+                                    <Form.Check.Input
+                                      type="radio"
+                                      value={item.value}
+                                      checked={item.value == this.state.value}
+                                      onChange={ (e) => this._change(e.target.value) }
+                                      className="control"
+                                    />
+                                    <Form.Check.Label htmlFor="t">{item.name}</Form.Check.Label>
+                                    {(item.value == this.state.value) ? <IndicatorWithAfter /> :<Indicator /> }
+
+                                  </Form.Check>
+
+
                                 // <Form.Check
                                 //     key={item.id}
                                 //     type='radio'
@@ -56,6 +102,7 @@ class RadioTypeOfControl extends Component {
                         })
                     }
                 </Form>
+
             </div>
         )
     }
